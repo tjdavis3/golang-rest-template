e599825c0b9270baa24cf1fae338454cc8ae1273
Todd Davis <tjdavis3@gmail.com>
2021-01-17 09:50:00 -0600

    chore: Cleanup configuration routine
    We were not using go-flags to it's full potential.
    This change removes the separate env loader and utilizes go-flags for it.
    Also adds better documentation to the help message

--- config/config.go
+++ config/config.go
@@ -1,25 +1,34 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	log "github.com/magna5/go-logger"
 	"github.com/magna5/go-logger/shims/zerolog"
 
 	sentry "github.com/getsentry/sentry-go"
 	flags "github.com/jessevdk/go-flags"
-	"github.com/joeshaw/envdecode"
-	"github.com/magna5/godotenv"
+
+	// Autoloads environment from .env
+	_ "github.com/joho/godotenv/autoload"
 )
+
+const progDesc = `
+Serves a REST API for ...
+`
 
 // Cfg configuration structure
 type Cfg struct {
-	Port                 int    `env:"PORT,default=8080" short:"p" long:"port" description:"HTTP Port"`
-	JwksCertRenewMinutes int    `env:"JWKS_RENEW_MINUTES,default=60" description:"Number of minutes to wait before renewing JWKS certificates"`
+	Port                 int    `env:"PORT" default:"8080" short:"p" long:"port" description:"HTTP Port"`
+	JwksCertRenewMinutes int    `env:"JWKS_RENEW_MINUTES" default:"60" description:"Number of minutes to wait before renewing JWKS certificates"`
 	JWTIssuer            string `env:"JWT_ISSUER" description:"The URL to the JWT issuing server"`
 }
 
 // Config is the current application configuration
 var Config = &Cfg{}
 
+// Configure creates the configuration for the application
 func Configure(args []string) *Cfg {
 	log.RootLogger = zerolog.New(nil)
 
@@ -28,17 +37,20 @@ func Configure(args []string) *Cfg {
 		log.Fatal("Error Initializing sentry: ", "error", err.Error())
 	}
 
-	err = godotenv.LoadOpt()
+	parser := flags.NewParser(Config, flags.Default)
+	// Uncomment the following line to add a custom usage statement
+	// parser.Usage = "[OPTIONS]"
+	parser.ShortDescription = "A REST API generated from the OpenAPI spec"
+	parser.LongDescription = progDesc
+	_, err = parser.Parse()
 	if err != nil {
-		panic(err)
-	}
-	err = envdecode.Decode(Config)
-	if err != nil {
-		panic(err)
-	}
-	_, err = flags.Parse(Config)
-	if err != nil {
-		panic(err)
+		if flgErr, ok := err.(*flags.Error); ok {
+			if flgErr.Type == flags.ErrHelp {
+				os.Exit(0)
+			}
+		}
+		fmt.Println(err.Error())
+		os.Exit(2)
 	}
 	return Config
 }
