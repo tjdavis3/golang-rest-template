286116eaff1028478b9db705ee6f98cfd0dbdb6f
Todd Davis <tjdavis3@gmail.com>
2021-03-10 13:50:29 -0600

    Add a new version of updImpl that is able to look at all files in package
    This version uses the ast and parse packages to look at the go
    source and determine if the functions have been implemented.

--- cmd/updImpl.go
+++ cmd/updImpl.go
@@ -1,121 +1,159 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
 	"log"
 	"os"
-	"reflect"
-	"regexp"
+	"path"
+	"path/filepath"
+	"runtime"
 	"strings"
 )
 
-const implFname = "serverImpl.go"
-const genFname = "api.gen.go"
-const funcSignature = `func (s *server) %s(w http.ResponseWriter, r *http.Request) {
-  http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
+const funcBody = `
+func (s *server) %s%s {
+	err := &ErrResponse{HTTPStatusCode: http.StatusNotImplemented}
+	err.Render(w, r)
 }
 `
 
-func itemExists(arrayType interface{}, item interface{}) bool {
-	arr := reflect.ValueOf(arrayType)
-
-	if arr.Kind() != reflect.Array && arr.Kind() != reflect.Slice {
-		panic(fmt.Sprintf("Invalid data-type: %s", arr.Kind()))
-	}
-
-	for i := 0; i < arr.Len(); i++ {
-		if arr.Index(i).Interface() == item {
-			return true
-		}
-	}
-
-	return false
-}
-
-func loadImpl(filename string) []string {
-	re := regexp.MustCompile(`\Afunc \(s \*server\) ([a-zA-Z0-9]+).*`)
-
-	var functions []string
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		line = strings.TrimSpace(line)
-		if re.MatchString(line) {
-			functions = append(functions, re.ReplaceAllString(line, "$1"))
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	return functions
-}
-
-func loadInterface(filename string) []string {
-	var functions []string
-
-	ifRE := regexp.MustCompile(`\Atype ServerInterface interface {`)
-	endRE := regexp.MustCompile(`.*}.*`)
-	funcRE := regexp.MustCompile(`^[[:blank:]]*([a-zA-Z0-9]+).*`)
-
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-
-	done := false
-	for scanner.Scan() {
-		if done {
-			break
-		}
-		line := scanner.Text()
-		if ifRE.MatchString(line) {
-			for scanner.Scan() {
-				line = strings.TrimSpace(scanner.Text())
-                                if strings.HasPrefix(line, "//") {
-                                        continue
-				}
-				if endRE.MatchString(line) {
-					break
-				}
-				if funcRE.MatchString(line) {
-					functions = append(functions, funcRE.ReplaceAllString(line, "$1"))
-				}
-			}
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	return functions
-}
 func main() {
-	functions := loadImpl(implFname)
-	intFuncs := loadInterface(genFname)
-	fd, err := os.OpenFile(implFname, os.O_WRONLY|os.O_APPEND, 0664)
+	_, fname, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Fatalln("Could not determine path")
+	}
+	dir := path.Dir(fname)
+	apiDir := path.Join(dir, "../api")
+	pkgs, err := parser.ParseDir(token.NewFileSet(), apiDir, filter, 0)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("%v\n", err)
+	}
+	fd, err := os.OpenFile(path.Join(apiDir, "serverImpl.go"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0660)
+	if err != nil {
+		panic(err)
 	}
 	defer fd.Close()
+	for _, pkg := range pkgs {
 
-	for _, intFunc := range intFuncs {
-		if !itemExists(functions, intFunc) {
-			line := fmt.Sprintf(funcSignature, intFunc)
-			log.Println("Adding ", line)
-			_, err = fd.WriteString(line)
-			if err != nil {
-				log.Fatal(err)
+		// Functions
+		si, functions := findFuncs(pkg)
+		for fn, sig := range si {
+			if value, exist := functions[fn]; !exist {
+				funcline := strings.Replace(sig, "func", "", 1)
+				fmt.Println("Adding ", fn)
+				_, err = fmt.Fprintf(fd, funcBody, fn, funcline)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Printf("Added function %s\n", fn)
+				}
+			} else {
+				if value != sig {
+					fmt.Printf("%s Signatures don't match\n", fn)
+					fmt.Println("  Interface: ", sig)
+					fmt.Println("  Function: ", value)
+				}
 			}
 		}
 	}
+}
+
+func findFuncs(pkg *ast.Package) (serverInterface map[string]string, functions map[string]string) {
+	functions = make(map[string]string)
+	serverInterface = make(map[string]string)
+	for _, f := range pkg.Files {
+		ast.Inspect(f, func(n ast.Node) bool {
+			switch t := n.(type) {
+			// find variable declarations
+			case *ast.TypeSpec:
+				// which are public
+				if t.Name.IsExported() {
+					switch t.Type.(type) {
+					// and are interfaces
+					case *ast.InterfaceType:
+						if t.Name.Name == "ServerInterface" {
+							intf := t.Type.(*ast.InterfaceType)
+							for _, m := range intf.Methods.List {
+								for _, field := range m.Names {
+									sig, err := getFuncSignature(field.Obj.Decl)
+									if err == nil {
+										serverInterface[field.Obj.Name] = sig
+									}
+								}
+							}
+						}
+					}
+				}
+			case *ast.FuncDecl:
+				sig, err := getFuncSignature(t)
+				if err == nil {
+					functions[t.Name.Name] = sig
+				}
+			}
+			return true
+		})
+	}
+	return serverInterface, functions
+}
+
+func getFuncSignature(intf interface{}) (signature string, err error) {
+	switch intf.(type) {
+	// and are interfaces
+	case *ast.FuncDecl:
+		found := false
+		fnc := intf.(*ast.FuncDecl)
+		if fnc.Recv != nil {
+			for _, fld := range fnc.Recv.List {
+				switch fld.Type.(type) {
+				case *ast.StarExpr:
+					expr := fld.Type.(*ast.StarExpr)
+					if fmt.Sprintf("%s", expr.X) == "server" {
+						found = true
+						break
+					}
+				}
+			}
+		}
+		var buf bytes.Buffer
+		if found {
+			body := fnc.Type
+			printer.Fprint(&buf, token.NewFileSet(), body)
+		} else {
+			err = errors.New("Function not found")
+		}
+		return buf.String(), err
+
+	case *ast.Field:
+		fnc := intf.(*ast.Field)
+		var buf bytes.Buffer
+		body := fnc.Type
+		printer.Fprint(&buf, token.NewFileSet(), body)
+		return buf.String(), nil
+	}
+	return
+}
+func filter(info os.FileInfo) bool {
+
+	name := info.Name()
+
+	if info.IsDir() {
+		return false
+	}
+
+	if filepath.Ext(name) != ".go" {
+		return false
+	}
+
+	if strings.HasSuffix(name, "_test.go") {
+		return false
+	}
+
+	return true
 
 }
